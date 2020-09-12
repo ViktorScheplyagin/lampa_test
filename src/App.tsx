ccdb33f8093dbddeb348658683b0676736d8ebdf
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-12 19:25:57 +0300

    added a route to a cart page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import 'styles/css_reset.css';
+
 import Home from 'pages/Home';
+import Cart from 'pages/Cart';
+
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
         </Route>
       </Switch>
     </BrowserRouter>
