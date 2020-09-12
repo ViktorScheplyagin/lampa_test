a523ab90291728c5e2d96768532ef973fd2190ee
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-12 19:30:12 +0300

    NavBar component created in a separate folder

---
+++ src/shared_components/NavBar/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.css';
+
+
+type NavBarProps = {
+  className?: string;
+}
+
+
+const NavBar: React.FC<NavBarProps> = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.pages_navigation}>
+        <NavLink to="/">
+          Home
+        </NavLink>
+      </div>
+      <NavLink to="/cart">
+        <div className={styles.cart_button}>Cart</div>
+      </NavLink>
+    </div>
+  )
+};
+
+export default NavBar;
