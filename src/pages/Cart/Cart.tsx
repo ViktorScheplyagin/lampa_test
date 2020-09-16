d48e9d83fda4a3f1da4940e8f12db52e06dfb29e
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-16 17:43:18 +0300

    added props types, helper functions moved to a separate file

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,71 +1,116 @@
-import React from 'react';
-import { ProductPayload } from 'services/api/products/model';
+import React, { useEffect } from 'react';
+import { ProductPayload, OrderPayload } from 'store/entities/model';
 import Layout from 'shared_components/Layout';
-import CartItem from './CartItem';
+import CartItem from './components/CartItem';
+import OrderForm from './components/OrderForm';
 
 import styles from './Cart.module.css';
 import commonStyles from 'styles/common.module.css';
-import OrderForm from './OrderForm';
+import { FormSubmitHandler } from 'redux-form';
+import spinner from 'assets/spinner.svg';
+import { calcTotalPrice, getItemsQuantities, onOrderSubmitDecorator } from './helpers';
 
-
-type CartProps = { itemsComponents: JSX.Element[]; }
-
-type CartContainerProps = {
-  items: ProductPayload[];
-  addItem: (product: ProductPayload) => void;
-  removeItem: (product: ProductPayload) => void;
+type CartProps = {
+  itemsComponents: JSX.Element[];
+  isProductsFetching: boolean;
+  totalPrice: number;
+  onOrderSubmit: FormSubmitHandler;
 };
 
+type CartContainerProps = {
+  productsLength: number;
+  items: ProductPayload[];
+  fetchProducts: () => void;
+  fetchCartItems: () => void;
+  addItem: (product: ProductPayload) => void;
+  removeItem: (product: ProductPayload) => void;
+  submitOrder: (order: OrderPayload) => void;
 
-const Cart: React.FC<CartProps> = ({ itemsComponents }) => (
+  isProductsFetching: boolean;
+  isProductAdding: boolean;
+  isProductRemoving: boolean;
+};
+
+const Cart: React.FC<CartProps> = ({
+  itemsComponents,
+  isProductsFetching,
+  onOrderSubmit,
+  totalPrice,
+}) => (
   <Layout>
     <div className={commonStyles.container}>
       <div className={styles.flex_container}>
         <div className={styles.items_container}>
-          {itemsComponents}
+          {isProductsFetching ? (
+            <div>
+              <img src={spinner} alt="spinner" />
+            </div>
+          ) : (
+            itemsComponents
+          )}
         </div>
-        
-        <OrderForm onSubmit={(values) => {
-          console.log(values);
-        }} />
+
+        <OrderForm onSubmit={onOrderSubmit} />
       </div>
+      {totalPrice && <div className={styles.cart_total_price}>Total: {`$${totalPrice}`}</div>}
     </div>
   </Layout>
 );
 
-const CartContainer: React.FC<CartContainerProps> = ({ items, addItem, removeItem }) => {
-  const itemsAndQuantities = {}; //TODO use Map here
-  const itemsComponents: JSX.Element[] = [];
+const CartContainer: React.FC<CartContainerProps> = ({
+  productsLength,
+  items,
+  isProductsFetching,
+  isProductAdding,
+  isProductRemoving,
+  fetchProducts,
+  fetchCartItems,
+  addItem,
+  removeItem,
+  submitOrder,
+}) => {
+  const itemsAndQuantities = getItemsQuantities(items);
 
-  items.forEach(item => {
-    if (itemsAndQuantities[item.id] === undefined) {
-      itemsAndQuantities[item.id] = {
-        item,
-        quantity: 1,
-      };
-    } else {
-      itemsAndQuantities[item.id].quantity += 1;
+  useEffect(() => {
+    if (productsLength === 0) {
+      fetchProducts();
     }
-  });
+    if (items.length === 0) {
+      fetchCartItems();
+    }
+    //eslint-disable-next-line
+  }, []);
 
-  for (let itemId in itemsAndQuantities) {
-    const { item, quantity } = itemsAndQuantities[itemId];
+  const mapItemsToComponents = () => {
+    const itemsComponents: JSX.Element[] = [];
 
-    itemsComponents.push(
-      <div key={item.id} className={styles.cart_item_container}>
-        <CartItem
-          item={item}
-          increment={addItem}
-          decrement={removeItem}
-          quantity={quantity}
-        />
-      </div>
-    );
+    for (let itemId in itemsAndQuantities) {
+      const { item, quantity } = itemsAndQuantities[itemId];
+
+      itemsComponents.push(
+        <div key={item.id} className={styles.cart_item_container}>
+          <CartItem
+            item={item}
+            increment={addItem}
+            decrement={removeItem}
+            quantity={quantity}
+            isDisabled={isProductAdding || isProductRemoving || isProductsFetching}
+          />
+        </div>,
+      );
+    }
+
+    return itemsComponents;
   };
 
   return (
-    <Cart itemsComponents={itemsComponents} />
+    <Cart
+      itemsComponents={mapItemsToComponents()}
+      onOrderSubmit={onOrderSubmitDecorator(itemsAndQuantities, submitOrder)}
+      isProductsFetching={isProductsFetching}
+      totalPrice={calcTotalPrice(items)}
+    />
   );
-}
+};
 
 export default CartContainer;
--- src/pages/Cart/index.ts
+++ src/pages/Cart/index.ts
@@ -1,13 +1,26 @@
 import Cart from 'pages/Cart/Cart';
 import { connect } from 'react-redux';
 import { AppStateType } from 'store';
-import { addProductToCart, removeProductFromCart } from 'store/resource/cart/actions';
+import { fetchProductsTC } from 'store/resource/home/thunks';
+import {
+  addProductToCartTC,
+  fetchCartItemsTC,
+  removeProductFromCartTC,
+  submitOrderTC,
+} from 'store/resource/cart/thunks';
 
-const mapStateToProps = (state: AppStateType) => ({
-  items: state.cart.items
+const mapStateToProps = ({ home, cart }: AppStateType) => ({
+  items: cart.items,
+  productsLength: home.products.length,
+  isProductsFetching: cart.isProductsFetching,
+  isProductAdding: cart.isProductAdding,
+  isProductRemoving: cart.isProductRemoving,
 });
 
 export default connect(mapStateToProps, {
-  addItem: addProductToCart,
-  removeItem: removeProductFromCart,
+  addItem: addProductToCartTC,
+  removeItem: removeProductFromCartTC,
+  submitOrder: submitOrderTC,
+  fetchProducts: fetchProductsTC,
+  fetchCartItems: fetchCartItemsTC,
 })(Cart);
