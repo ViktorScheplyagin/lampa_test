3259514f3827e83129a32a7b73e32dec1c032134
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-16 17:52:34 +0300

    added state toggle action creators

--- src/store/resource/cart/actions.ts
+++ src/store/resource/cart/actions.ts
@@ -1,14 +1,50 @@
-import { ProductPayload } from "services/api/products/model";
+import { OrderPayload, ProductPayload } from 'store/entities/model';
 
-export const ADD_PRODUCT_TO_CART = "ADD_PRODUCT_TO_CART";
-export const REMOVE_PRODUCT_FROM_CART = "REMOVE_PRODUCT_FROM_CART";
+export const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
+export const REMOVE_PRODUCT_FROM_CART = 'REMOVE_PRODUCT_FROM_CART';
+export const SUBMIT_ORDER = 'SUBMIT_ORDER';
+export const TOGGLE_IS_ORDER_SUBMITTING = 'TOGGLE_IS_ORDER_SUBMITTING';
+export const TOGGLE_IS_PRODUCTS_FETCHING = 'TOGGLE_IS_PRODUCTS_FETCHING';
+export const TOGGLE_IS_PRODUCT_ADDING = 'TOGGLE_IS_PRODUCT_ADDING';
+export const TOGGLE_IS_PRODUCT_REMOVING = 'TOGGLE_IS_PRODUCT_REMOVING';
+export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS';
 
-export const addProductToCart = (product: ProductPayload) => ({
+export const fetchCartItemsAC = (items: ProductPayload[]) => ({
+  type: FETCH_CART_ITEMS,
+  payload: items,
+});
+
+export const addProductToCartAC = (product: ProductPayload) => ({
   type: ADD_PRODUCT_TO_CART,
   payload: product,
 });
 
-export const removeProductFromCart = (product: ProductPayload) => ({
+export const removeProductFromCartAC = (product: ProductPayload) => ({
   type: REMOVE_PRODUCT_FROM_CART,
   payload: product,
+});
+
+export const submitOrderAC = (order: OrderPayload) => ({
+  type: SUBMIT_ORDER,
+  payload: order,
+});
+
+export const toggleIsOrderSubmittingAC = (flag: boolean) => ({
+  type: TOGGLE_IS_ORDER_SUBMITTING,
+  payload: flag,
+});
+
+export const toggleIsProductsFetchingAC = (flag: boolean) => ({
+  type: TOGGLE_IS_PRODUCTS_FETCHING,
+  payload: flag,
+});
+
+export const toggleIsProductAddingAC = (flag: boolean) => ({
+  type: TOGGLE_IS_PRODUCT_ADDING,
+  payload: flag,
+});
+
+export const toggleIsProductRemovingAC = (flag: boolean) => ({
+  type: TOGGLE_IS_PRODUCT_REMOVING,
+  payload: flag,
 });
--- src/store/resource/cart/reducer.ts
+++ src/store/resource/cart/reducer.ts
@@ -1,35 +1,85 @@
-import { Action } from "redux";
-import { ProductPayload } from "services/api/products/model";
-import { ADD_PRODUCT_TO_CART, REMOVE_PRODUCT_FROM_CART } from "./actions"
+import { ProductPayload } from 'store/entities/model';
+import {
+  ADD_PRODUCT_TO_CART,
+  FETCH_CART_ITEMS,
+  REMOVE_PRODUCT_FROM_CART,
+  TOGGLE_IS_ORDER_SUBMITTING,
+  TOGGLE_IS_PRODUCTS_FETCHING,
+  TOGGLE_IS_PRODUCT_ADDING,
+  TOGGLE_IS_PRODUCT_REMOVING,
+} from './actions';
 
+type StateType = {
+  items: ProductPayload[];
+  isOrderSubmitting: boolean;
+  isProductsFetching: boolean;
+  isProductAdding: boolean;
+  isProductRemoving: boolean;
+};
 
-type StateType = { items: ProductPayload[]; };
-
-type ActionType = Action & { payload: ProductPayload; };
-
+type ActionType = {
+  type: string;
+  payload: any;
+};
 
 const initialState: StateType = {
   items: [],
+  isOrderSubmitting: false,
+  isProductsFetching: false,
+  isProductAdding: false,
+  isProductRemoving: false,
 };
 
 const cartReducer = (state = initialState, action: ActionType) => {
-  switch(action.type) {
+  switch (action.type) {
     case ADD_PRODUCT_TO_CART:
       return {
         ...state,
-        items: [...state.items, action.payload]
+        items: [...state.items, action.payload],
       };
+
     case REMOVE_PRODUCT_FROM_CART:
       const { items } = state;
       const targetIdx = items.indexOf(action.payload);
 
       return {
         ...state,
-        items: items.filter((item, idx) => idx !== targetIdx)
+        items: items.filter((item, idx) => idx !== targetIdx),
       };
+
+    case FETCH_CART_ITEMS:
+      return {
+        ...state,
+        items: action.payload,
+      };
+
+    case TOGGLE_IS_ORDER_SUBMITTING:
+      return {
+        ...state,
+        isOrderSubmitting: action.payload,
+      };
+
+    case TOGGLE_IS_PRODUCTS_FETCHING:
+      return {
+        ...state,
+        isProductsFetching: action.payload,
+      };
+
+    case TOGGLE_IS_PRODUCT_ADDING:
+      return {
+        ...state,
+        isProductAdding: action.payload,
+      };
+
+    case TOGGLE_IS_PRODUCT_REMOVING:
+      return {
+        ...state,
+        isProductRemoving: action.payload,
+      };
+
     default:
       return state;
-  };
+  }
 };
 
 export default cartReducer;
