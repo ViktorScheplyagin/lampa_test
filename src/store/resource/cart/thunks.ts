24ab6cde1247a7fa5829b6f7e3c25f95625dea2c
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-16 17:53:05 +0300

    created the cart thunks

---
+++ src/store/resource/cart/thunks.ts
@@ -0,0 +1,43 @@
+import { OrderPayload, ProductPayload } from 'store/entities/model';
+import { deleteItemFromCart, getAllCartItems, postItemToCart, postOrder } from 'services/api';
+import {
+  addProductToCartAC,
+  fetchCartItemsAC,
+  removeProductFromCartAC,
+  toggleIsOrderSubmittingAC,
+  toggleIsProductAddingAC,
+  toggleIsProductRemovingAC,
+  toggleIsProductsFetchingAC,
+} from 'store/resource/cart/actions';
+
+export const submitOrderTC = (order: OrderPayload) => async (dispatch) => {
+  dispatch(toggleIsOrderSubmittingAC(true));
+
+  await postOrder(order);
+
+  dispatch(toggleIsOrderSubmittingAC(false));
+};
+
+export const fetchCartItemsTC = () => async (dispatch) => {
+  dispatch(toggleIsProductsFetchingAC(true));
+
+  const cartItemsJSON = await getAllCartItems();
+  const cartItems: ProductPayload[] = JSON.parse(cartItemsJSON as string);
+
+  dispatch(toggleIsProductsFetchingAC(false));
+  dispatch(fetchCartItemsAC(cartItems));
+};
+
+export const addProductToCartTC = (product: ProductPayload) => async (dispatch) => {
+  dispatch(toggleIsProductAddingAC(true));
+  await postItemToCart(product);
+  dispatch(addProductToCartAC(product));
+  dispatch(toggleIsProductAddingAC(false));
+};
+
+export const removeProductFromCartTC = (product: ProductPayload) => async (dispatch) => {
+  dispatch(toggleIsProductRemovingAC(true));
+  await deleteItemFromCart(product.id);
+  dispatch(removeProductFromCartAC(product));
+  dispatch(toggleIsProductRemovingAC(false));
+};
