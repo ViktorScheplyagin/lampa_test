f7de7f3e655eeab99c45ae9a541bf4975add7505
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-16 17:44:34 +0300

    added a spinner icon, actions

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,42 +1,66 @@
 import React, { useEffect } from 'react';
 import Layout from 'shared_components/Layout';
-import ProductItem from 'pages/Home/ProductItem';
-import { ProductPayload } from 'services/api/products/model';
+import ProductItem from 'pages/Home/components/ProductItem';
+import { ProductPayload } from 'store/entities/model';
 
 import styles from './Home.module.css';
 import commonStyles from 'styles/common.module.css';
-
+import spinner from 'assets/spinner.svg';
 
 type HomeProps = {
   products: ProductPayload[];
+  isProductsFetching: boolean;
+  isProductAddingToCart: boolean;
   addToCart: (product: ProductPayload) => void;
 };
 
 type HomeContainerProps = HomeProps & {
+  cartItemsLength: number;
   fetchProducts: () => void;
+  fetchCartItems: () => void;
 };
 
-
-const Home: React.FC<HomeProps> = ({ products, addToCart }) => (
+const Home: React.FC<HomeProps> = ({
+  products,
+  addToCart,
+  isProductsFetching,
+  isProductAddingToCart,
+}) => (
   <Layout>
     <div className={commonStyles.container}>
-      <div className={styles.grid_layout}>
-        {products?.map(product => (
-          <ProductItem 
-            key={product.id} 
-            product={product} 
-            onAddToCart={addToCart}
-          />
-        ))}
-      </div>
+      {isProductsFetching ? (
+        <div className={styles.spinner_wrapper}>
+          <img src={spinner} alt="spinner" />
+        </div>
+      ) : (
+        <div className={styles.grid_layout}>
+          {products?.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              onAddToCart={addToCart}
+              isDisabled={isProductAddingToCart}
+            />
+          ))}
+        </div>
+      )}
     </div>
   </Layout>
 );
 
-
-const HomeContainer: React.FC<HomeContainerProps> = ({fetchProducts, ...homeProps}) => {
+const HomeContainer: React.FC<HomeContainerProps> = ({
+  fetchProducts,
+  fetchCartItems,
+  cartItemsLength,
+  ...homeProps
+}) => {
   useEffect(() => {
-    fetchProducts();
+    if (homeProps.products.length === 0) {
+      fetchProducts();
+    }
+    if (cartItemsLength === 0) {
+      fetchCartItems();
+    }
     //eslint-disable-next-line
   }, []);
 
--- src/pages/Home/index.ts
+++ src/pages/Home/index.ts
@@ -1,15 +1,19 @@
 import Home from './Home';
 import { connect } from 'react-redux';
 
-import { addProductToCart } from 'store/resource/cart/actions';
-import { fetchNewProducts } from 'store/resource/home/actions';
 import { AppStateType } from 'store';
+import { fetchProductsTC } from 'store/resource/home/thunks';
+import { addProductToCartTC, fetchCartItemsTC } from 'store/resource/cart/thunks';
 
-const mapStateToProps = (state: AppStateType) => ({
-  products: state.home.products,
+const mapStateToProps = ({ home, cart }: AppStateType) => ({
+  products: home.products,
+  cartItemsLength: cart.items.length,
+  isProductsFetching: home.isProductsFetching,
+  isProductAddingToCart: cart.isProductAdding,
 });
 
 export default connect(mapStateToProps, {
-  addToCart: addProductToCart,
-  fetchProducts: fetchNewProducts,
+  addToCart: addProductToCartTC,
+  fetchProducts: fetchProductsTC,
+  fetchCartItems: fetchCartItemsTC,
 })(Home);
