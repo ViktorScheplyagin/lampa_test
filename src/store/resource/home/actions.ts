f930f751aa78593b12348f7e73a846ffae18ebe6
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-16 17:54:11 +0300

    fetchNewProducts action creator done; toggleIsProductsFetching action creator is created

--- src/store/resource/home/actions.ts
+++ src/store/resource/home/actions.ts
@@ -1,6 +1,15 @@
-export const FETCH_NEW_PRODUCTS = "FETCH_NEW_PRODUCTS";
+import { ProductPayload } from 'store/entities/model';
 
-export const fetchNewProducts = () => ({
+export const FETCH_NEW_PRODUCTS = 'FETCH_NEW_PRODUCTS';
+export const TOGGLE_IS_PRODUCTS_FETCHING = 'TOGGLE_IS_PRODUCTS_FETCHING';
+export const TOGGLE_IS_PRODUCT_ADDING_TO_CART = 'TOGGLE_IS_PRODUCT_ADDING_TO_CART';
+
+export const fetchNewProductsAC = (products: ProductPayload[]) => ({
   type: FETCH_NEW_PRODUCTS,
-  payload: ["new"]
+  payload: products,
+});
+
+export const toggleIsProductsFetchingAC = (flag: boolean) => ({
+  type: TOGGLE_IS_PRODUCTS_FETCHING,
+  payload: flag,
 });
