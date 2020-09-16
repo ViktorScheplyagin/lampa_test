64435c92b5436b67fd0b11c7098850b979619860
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-16 17:57:53 +0300

    added the fake DB initialization (in localStorage)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
@@ -7,9 +7,17 @@ import 'styles/css_reset.css';
 
 import Home from 'pages/Home';
 import Cart from 'pages/Cart';
-
+import mockData from 'services/mockData';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const allMockProductsJSON = localStorage.getItem('all');
+    const cartMockProductsJSON = localStorage.getItem('cart');
+
+    if (!allMockProductsJSON) localStorage.setItem('all', mockData);
+    if (!cartMockProductsJSON) localStorage.setItem('cart', JSON.stringify([]));
+  }, []);
+
   return (
     <Provider store={store}>
       <BrowserRouter>
@@ -23,7 +31,7 @@ const App: React.FC = () => {
         </Switch>
       </BrowserRouter>
     </Provider>
-  )
+  );
 };
 
 export default App;
