e5bb284ec142e35da25927c032f49c710c69d9b6
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-16 17:56:30 +0300

    the app store improved to use the redux-thunk middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,16 @@
-import { createStore, combineReducers } from "redux";
-import cartReducer from "./resource/cart/reducer";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import cartReducer from './resource/cart/reducer';
 import homeReducer from './resource/home/reducer';
+import { reducer as orderFormReducer } from 'redux-form';
+import thunkMiddleware from 'redux-thunk';
 
 const rootReducer = combineReducers({
   home: homeReducer,
   cart: cartReducer,
+  form: orderFormReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 type RootReducerType = typeof rootReducer;
 export type AppStateType = ReturnType<RootReducerType>;
