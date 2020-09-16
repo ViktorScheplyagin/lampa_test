8ef4f3f56f4d60b464e70c6b61c032b54ef7de33
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-16 20:26:38 +0300

    Updates

---
+++ precache-manifest.95d241f4ce81cfa74c6c69f7e2e51e0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bcd5130bcd9bb9dffcba306d266f9d",
+    "url": "/lampa_test/index.html"
+  },
+  {
+    "revision": "91ee0359e2bfc5b9fbf7",
+    "url": "/lampa_test/static/css/main.5a56539f.chunk.css"
+  },
+  {
+    "revision": "b5b093c052ac3a6efa4f",
+    "url": "/lampa_test/static/js/2.93a49791.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lampa_test/static/js/2.93a49791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ee0359e2bfc5b9fbf7",
+    "url": "/lampa_test/static/js/main.6648cb55.chunk.js"
+  },
+  {
+    "revision": "c02631a99dd36a810c5a",
+    "url": "/lampa_test/static/js/runtime-main.a3f9292a.js"
+  },
+  {
+    "revision": "07073b21072df2feeca763ae804678f0",
+    "url": "/lampa_test/static/media/spinner.07073b21.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lampa_test/precache-manifest.d3b11ab642525a206d7d0eb6fa289407.js"
+  "/lampa_test/precache-manifest.95d241f4ce81cfa74c6c69f7e2e51e0f.js"
 );
 
 self.addEventListener('message', (event) => {
