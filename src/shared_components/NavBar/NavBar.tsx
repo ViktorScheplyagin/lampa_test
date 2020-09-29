96761d04660fa67bcab6439d1cd247e41b4e4af3
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-29 21:02:58 +0300

    NavBar component refactored to chakra ui using

--- src/shared_components/NavBar/NavBar.tsx
+++ src/shared_components/NavBar/NavBar.tsx
@@ -1,32 +1,25 @@
 import React from 'react';
+import { Flex } from '@chakra-ui/core';
 import { NavLink } from 'react-router-dom';
-import Button from 'shared_components/Button';
-import styles from './NavBar.module.css';
 
 type NavBarProps = {
-  totalCartItemsPrice: number;
+  textColor?: string;
+  width?: string;
 };
 
-const NavBar: React.FC<NavBarProps> = ({ totalCartItemsPrice }) => {
+const NavBar: React.FC<NavBarProps> = ({ width, textColor }) => {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.pages_navigation}>
-        <NavLink to="/">Home</NavLink>
-      </div>
-      <div className={styles.tocart_link_wrapper}>
-        <div>{totalCartItemsPrice ? `Total: $${totalCartItemsPrice}` : null}</div>
-        <NavLink to="/cart">
-          <Button
-            style={{
-              height: '2.5em',
-              width: '5em',
-            }}
-          >
-            Cart
-          </Button>
-        </NavLink>
-      </div>
-    </div>
+    <Flex
+      justify="space-between"
+      align="center"
+      w={width || "100%"}
+      h="3.5em"
+      p="1em"
+      boxSizing="border-box"
+      color={textColor || "gray.900"}
+    >
+      <NavLink to="/">Home</NavLink>
+    </Flex>
   );
 };
 
