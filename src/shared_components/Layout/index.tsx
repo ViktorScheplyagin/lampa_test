ebf57a1042d988182b9455b82de0051a02b39bb8
Viktor Scheplyagin <silverman995@gmail.com>
2020-09-29 21:03:21 +0300

    added Footer, removed styles module import

--- src/shared_components/Layout/index.tsx
+++ src/shared_components/Layout/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Footer from './Footer';
 import Header from './Header';
-import styles from './Layout.module.css';
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Header />
-      <div className={styles.page_layout_content}>{children}</div>
+      {children}
+      <Footer />
     </>
   );
 };
