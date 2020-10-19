2a13017d3ecd0d2fadddad8532b400e9d93dbf36
Viktor Scheplyagin <silverman995@gmail.com>
2020-10-20 01:10:54 +0300

    the Cart page was rebuilt

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'shared_components/Layout';
+
+const Cart: React.FC = () => {
+  return (
+    <Layout>
+      Here will be a cart page
+    </Layout>
+  )
+};
+
+export default Cart;
