070f9383bafd7fe92d9c7ba2912d1b4d11d3e726
Viktor Scheplyagin <silverman995@gmail.com>
2020-10-20 01:14:18 +0300

    redux-form removed

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import cartReducer from './resource/cart/reducer';
 import homeReducer from './resource/home/reducer';
-import { reducer as orderFormReducer } from 'redux-form';
 import thunkMiddleware from 'redux-thunk';
 
 const rootReducer = combineReducers({
   home: homeReducer,
   cart: cartReducer,
-  form: orderFormReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
