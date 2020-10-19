9749d6dad00b8a6094a1acb8f2d701d5f82666d5
Viktor Scheplyagin <silverman995@gmail.com>
2020-10-20 01:16:41 +0300

    Updates

---
+++ precache-manifest.1b1776f235e6babb13543763bb464fab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb1fbd05c3ca3d3b1cc981148b3f5c8",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "79c8c923d157e983b638",
+    "url": "/shop/static/js/2.85415830.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shop/static/js/2.85415830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943e408d0cd6898a7105",
+    "url": "/shop/static/js/main.8768dbee.chunk.js"
+  },
+  {
+    "revision": "aa1945b71dc1f6fb53f2",
+    "url": "/shop/static/js/runtime-main.4a8e269c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shop/precache-manifest.7ec388cad4f920f75c57ec6fb49ee778.js"
+  "/shop/precache-manifest.1b1776f235e6babb13543763bb464fab.js"
 );
 
 self.addEventListener('message', (event) => {
