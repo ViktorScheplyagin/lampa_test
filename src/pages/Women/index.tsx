900677b64f90cef8a56fe07dc397400ef14be208
Viktor Scheplyagin <silverman995@gmail.com>
2020-10-20 01:13:04 +0300

    the initial Women page

---
+++ src/pages/Women/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'shared_components/Layout';
+
+const Women: React.FC = () => {
+  return (
+    <Layout>
+      Here will be a Women page
+    </Layout>
+  )
+};
+
+export default Women;
